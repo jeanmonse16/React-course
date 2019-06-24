bf790547ada7b86b385fe5d22d72f254aa90bfea
jeanpier <monserrateluisje@gmail.com>
2019-06-24 00:58:34 -0400

    levantando los estados

--- src/components/Badgeform.js
+++ src/components/Badgeform.js
@@ -2,28 +2,22 @@ import React from "react";
 
 class Badgeform extends React.Component{
     
-    state = {
-        firstName: "",
-        LastName: "",
-        email: "",
-        jobPosition: "",
-        twitter: ""
-    }
+  
 
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    };
+//handleChange = e => {
+        //this.setState({
+           // [e.target.name]: e.target.value
+     //   })
+  //  };
 
-    handleClick = e => {
-        console.log("boton presionado")
-        console.log(this.state)
-    }
+//handleClick = e => {
+       // console.log("boton presionado")
+      //  console.log(this.state)
+   // }
 
-    handleSubmit = e => {
-        console.log(this.state)
-    }
+   // handleSubmit = e => {
+        //console.log(this.state)
+   //}
 
     render(){
         return(
@@ -33,29 +27,29 @@ class Badgeform extends React.Component{
                 <form>
                     <div className="form-group">
                         <label> First Name</label>
-                        <input value={this.state.firstName} onChange={this.handleChange} className="form-control" type="text" name="firstName" ></input>
+                        <input value={this.props.formValues.firstName} onChange={this.props.onChange} className="form-control" type="text" name="firstName" ></input>
                     </div>
                     
                     <div className="form-group">
                         <label> Last Name</label>
-                        <input value={this.state.LastName} onChange={this.handleChange} className="form-control" type="text" name="LastName" ></input>
+                        <input value={this.props.formValues.LastName} onChange={this.props.onChange} className="form-control" type="text" name="LastName" ></input>
                     </div>
 
                     <div className="form-group">
                         <label> Email</label>
-                        <input value={this.state.email} onChange={this.handleChange} className="form-control" type="email" name="email" ></input>
+                        <input value={this.props.formValues.email} onChange={this.props.onChange} className="form-control" type="email" name="email" ></input>
                     </div>
 
                     <div className="form-group">
                         <label> job position</label>
-                        <input value={this.state.jobPosition} onChange={this.handleChange} className="form-control" type="text" name="jobPosition" ></input>
+                        <input value={this.props.formValues.jobPosition} onChange={this.props.onChange} className="form-control" type="text" name="jobPosition" ></input>
                     </div>
 
                     <div className="form-group">
                         <label> twitter</label>
-                        <input value={this.state.twitter} onChange={this.handleChange} className="form-control" type="text" name="twitter" ></input>
+                        <input value={this.props.formValues.twitter} onChange={this.props.onChange} className="form-control" type="text" name="twitter" ></input>
                     </div>
-                    <button onClick={this.handleClick} className="btn btn-primary" type="button">Save</button>
+                    <button onClick={this.props.onClick} className="btn btn-primary" type="button">Save</button>
                 </form>
             </div>
         )
--- src/pages/Badgenew.js
+++ src/pages/Badgenew.js
@@ -6,6 +6,32 @@ import Badge from "../components/Badge";
 import Badgeform from "../components/Badgeform";
 
 class Badgenew extends React.Component{
+
+    state= { 
+        form:{
+               firstName:"",
+               LastName:"",
+               email:"",
+               jobPosition:"",
+               twitter:"",
+             } 
+    };
+   
+
+    handleChange = e => {
+        this.setState({
+            form:{
+            ...this.state.form,
+            [e.target.name]: e.target.value
+            },
+        })
+    }
+
+    handleClick = e => {
+        console.log(this.state.form)
+        console.log("boton presionado")
+    }
+
     render()
     {
        return (
@@ -18,10 +44,10 @@ class Badgenew extends React.Component{
          <div className="container">
            <div className="row">
                <div className="col-6">
-                     <Badge firstname="Jeanpier" lastname="Monserrate" hashtag="platziconf" />
+                     <Badge firstname={this.state.form.firstName} lastname={this.state.form.LastName} hashtag="platziconf" />
                </div>
                <div className="col-6">
-                     <Badgeform/>
+                     <Badgeform onChange={this.handleChange} formValues={this.state.form} onClick={this.handleClick} />
                </div>
            </div>
          </div>
