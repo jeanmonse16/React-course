73e7b12612d7ba1f1d1ebc734f65537909c2aba9
jeanpier <monserrateluisje@gmail.com>
2019-07-17 12:22:34 -0400

    UI COMPONENTS Y CONTAINER COMPONENTS

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Layout from "./Layout";
 import Notfound  from "../pages/Notfound";
 import Home from "../pages/Home";
 import BadgeEdit from "../pages/BadgeEdit";
-import BadgeDetails from "../pages/BadgeDetails"
+import BadgeDetails from "../pages/BadgeDetailsContainer"
 
 function App (){
    return (
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,98 +1,63 @@
-import React from "react";
+import React from "react"
+
+import {Link} from "react-router-dom"
 
 import "./styles/BadgeDetails.css";
 import platziconf from "../images/platziconf-logo.svg";
 
-import api from "../api.js"
-import PageLoading from "../components/PageLoading"
-import PageError from "../components/PageError"
 import Badge from "../components/Badge"
-import {Link} from "react-router-dom"
 
-class BadgeDetails extends React.Component{
+function BadgeDetails (props){
 
- state = {
-     loading: true,
-     error: null,
-     data: undefined
- }
-
- componentDidMount(){
-     this.fetchData()
- }
+    const badge = props.badge
  
- fetchData = async e => {
-     this.setState({loading: true, error: null})
+    return (
+        <div>
+            <div className="BadgeDetails__hero">
 
-     try{
-         const data = await api.badges.read(this.props.match.params.badgeId)
-         this.setState({
-             loading: false, data
-         })
-     }
-     catch(error){
-        this.setState({
-            loading: false, error
-        })
-     }
- }
-
- render(){
-     if(this.state.loading){
-         return <PageLoading />
-     }
-     if(this.state.error){
-         return <PageError error={this.state.error}/>
-     }
-
-     const badge = this.state.data
-     return (
-         <div>
-             <div className="BadgeDetails__hero">
-
-                <div className="container">
-                  <div className="row">
-                    <div className="col-6">
-                       <img src={platziconf} alt="logo de la conferencia" />
-                    </div>
-                    <div className="col-6 BadgeDetails__hero-attendant-name">
-                       <h1>
-                           {badge.firstName} {badge.lastName}
-                       </h1>
-                    </div>
-                  </div>
-                </div>
-
-             </div>
-             <div className="container">
+               <div className="container">
                  <div className="row">
-                    <div className="col">
-                        <Badge 
-                        firstName={badge.firstName}
-                        lastName={badge.lastName}
-                        email={badge.email}
-                        jobTitle={badge.jobTitle}
-                        twitter={badge.twitter}
-                        />
-                    </div>
-                    <div className="col">
-                       <div>
-                       <h2>
-                           Actions
-                       </h2>
-                       </div>
-                       <div>
-                       <Link to={`/badges/${badge.id}/edit`} className="btn btn-primary mb-4">Edit</Link>
-                       </div>
-                       <div>
-                       <button className="btn btn-danger">Delete</button>
-                       </div>
-                    </div>
+                   <div className="col-6">
+                      <img src={platziconf} alt="logo de la conferencia" />
+                   </div>
+                   <div className="col-6 BadgeDetails__hero-attendant-name">
+                      <h1>
+                          {badge.firstName} {badge.lastName}
+                      </h1>
+                   </div>
                  </div>
-             </div>
-         </div>
-     )
- }
-}
+               </div>
 
-export default BadgeDetails;
+            </div>
+            <div className="container">
+                <div className="row">
+                   <div className="col">
+                       <Badge 
+                       firstName={badge.firstName}
+                       lastName={badge.lastName}
+                       email={badge.email}
+                       jobTitle={badge.jobTitle}
+                       twitter={badge.twitter}
+                       />
+                   </div>
+                   <div className="col">
+                      <div>
+                      <h2>
+                          Actions
+                      </h2>
+                      </div>
+                      <div>
+                      <Link to={`/badges/${badge.id}/edit`} className="btn btn-primary mb-4">Edit</Link>
+                      </div>
+                      <div>
+                      <button className="btn btn-danger">Delete</button>
+                      </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    )
+
+ }
+
+ export default BadgeDetails;
