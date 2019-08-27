57655be5f3d778fa4d307aef9379b8d515d6ab74
jeanpier <monserrateluisje@gmail.com>
2019-08-27 13:01:56 -0400

    Updates

---
+++ precache-manifest.a3faa373f7b2786e331d223c867f3917.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/CowList/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "5358499a41d0be6a121a",
+    "url": "/CowList/static/css/main.5da2279c.chunk.css"
+  },
+  {
+    "revision": "94954d4b47511cc9c02f",
+    "url": "/CowList/static/js/runtime~main.94954d4b.js"
+  },
+  {
+    "revision": "18a3471d5a3ef49116be588f60c58611",
+    "url": "/CowList/static/media/cowlist-logo.18a3471d.svg"
+  },
+  {
+    "revision": "7ab583dd998f42cdfdc8",
+    "url": "/CowList/static/js/2.7ab583dd.chunk.js"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/CowList/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "5358499a41d0be6a121a",
+    "url": "/CowList/static/js/main.5358499a.chunk.js"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/CowList/static/media/descarga.ed7c58ea.svg"
+  },
+  {
+    "revision": "ecb7533704935eee76784e83225d1cbd",
+    "url": "/CowList/static/media/logovaca.ecb75337.svg"
+  },
+  {
+    "revision": "258ee7f1c41640c404900ec5d39c1617",
+    "url": "/CowList/static/media/vacagrande.258ee7f1.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/CowList/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "7ab583dd998f42cdfdc8",
+    "url": "/CowList/static/css/2.b34bf7a3.chunk.css"
+  },
+  {
+    "revision": "44c51c29fb98e1996fafba2152c1cce5",
+    "url": "/CowList/index.html"
+  }
+];
